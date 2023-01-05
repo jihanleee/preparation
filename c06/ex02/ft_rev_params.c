dae052f42b0cc23df8cf03bcccebabe3eba08f96
jihanleee <sbll22006@naver.com>
2023-01-05 18:12:42 +0100

    c06 completed

--- c06/ex02/ft_rev_params.c
+++ c06/ex02/ft_rev_params.c
@@ -6,7 +6,7 @@
 /*   By: jihanleee <sbll22006@naver.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 23:16:37 by jihanleee         #+#    #+#             */
-/*   Updated: 2023/01/04 23:31:51 by jihanleee        ###   ########.fr       */
+/*   Updated: 2023/01/05 18:10:16 by jihanleee        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -25,11 +25,11 @@ int	main(int argc, char *argv[])
 {
 	int	i;
 
-	i = 1;
-	while (i < argc)
+	i = argc - 1;
+	while (i >= 1)
 	{
 		ft_putstr(argv[i]);
-		i++;
+		i--;
 	}
 	return (0);
 }
